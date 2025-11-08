165fc84eb8f7a95e5f47301f8e0c9387f1d54665
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-08 18:40:58 -0300

    add generic entity class

---
+++ KluskaStore.Domain/Entities/Entity.cs
@@ -0,0 +1,31 @@
+﻿namespace KluskaStore.Domain.Entities;
+
+public abstract class Entity<TId> {
+  public TId Id { get; protected set; }
+
+  protected Entity() { }
+
+  protected Entity(TId id) {
+    Id = id;
+  }
+
+  public override bool Equals(object? obj) {
+    if (obj is not Entity<TId> other) return false;
+    if (ReferenceEquals(this, other)) return true;
+    return EqualityComparer<TId>.Default.Equals(Id, other.Id);
+  }
+
+  public override int GetHashCode() {
+    return EqualityComparer<TId>.Default.GetHashCode(Id!);
+  }
+
+  public static bool operator ==(Entity<TId>? a, Entity<TId>? b) {
+    if (a is null && b is null) return true;
+    if (a is null || b is null) return false;
+    return a.Equals(b);
+  }
+
+  public static bool operator !=(Entity<TId>? a, Entity<TId>? b) {
+    return !(a == b);
+  }
+}
