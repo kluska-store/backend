e6571d7748dea76f0e6e09b87e4127e27934efed
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-08 19:01:37 -0300

    feat(infra): add DbContext with basic constructor

---
+++ KluskaStore.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using KluskaStore.Domain.Entities;
+
+namespace KluskaStore.Infrastructure.Data;
+
+public class AppDbContext : DbContext {
+  public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder) {
+    base.OnModelCreating(modelBuilder);
+    
+    // TODO: adicionar os mapeamentos com Fluent API
+  }
+}
