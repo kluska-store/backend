9842aec72aa94a8b76ae7054f633cb0857e10191
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-08 19:31:47 -0300

    feat(api): configure Swagger use and remove sample code from Program.cs

--- KluskaStore.API/Program.cs
+++ KluskaStore.API/Program.cs
@@ -1,48 +1,38 @@
 using KluskaStore.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// --- CONFIGURAÇÃO DO EF CORE ---
 builder.Services.AddDbContext<AppDbContext>(options =>
   options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+// --- CONFIGURAÇÃO DE CONTROLLERS E SWAGGER ---
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(options => 
+  options.SwaggerDoc("v1", new OpenApiInfo {
+    Title = "KluskaStore API",
+    Version = "v1",
+    Description = "API RESTful do sistema de marketplace KluskaStore"
+  })
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// --- CONFIGURAÇÃO DO PIPELINE ---
 if (app.Environment.IsDevelopment()) {
-  app.MapOpenApi();
+  app.UseSwagger();
+  app.UseSwaggerUI(options => {
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "KluskaStore API v1");
+    options.RoutePrefix = string.Empty;
+  });
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[] {
-  "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () => {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-          DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-          Random.Shared.Next(-20, 55),
-          summaries[Random.Shared.Next(summaries.Length)]
-        ))
-      .ToArray();
-    return forecast;
-  })
-  .WithName("GetWeatherForecast");
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary) {
-  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
