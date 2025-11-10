122fe7709da5fa7f1d0863a70c787b4a019d6137
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-10 17:49:41 -0300

    feat(db context): add mapping for Store

--- KluskaStore.Infrastructure/Data/AppDbContext.cs
+++ KluskaStore.Infrastructure/Data/AppDbContext.cs
@@ -7,6 +7,7 @@ public class AppDbContext : DbContext {
   public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
   
   public DbSet<Product> Products { get; set; } // sample
+  public DbSet<Store> Stores { get; set; }
 
   protected override void OnModelCreating(ModelBuilder modelBuilder) {
     base.OnModelCreating(modelBuilder);
