646a0abfbbfb6ea6d5838ceb0131281bd4bdcf0a
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-10 17:26:45 -0300

    feat(repository): add generic entity repository

---
+++ KluskaStore.Domain/Repositories/IEntityRepository.cs
@@ -0,0 +1,11 @@
+﻿using KluskaStore.Domain.Interfaces;
+
+namespace KluskaStore.Domain.Repositories;
+
+public interface IEntityRepository<T, in TId> where T : class, IEntity {
+  Task<T?> GetByIdAsync(TId id);
+  Task<IEnumerable<T>> GetAllAsync();
+  Task AddAsync(T entity);
+  Task UpdateAsync(T entity);
+  Task DeleteAsync(T entity);
+}
