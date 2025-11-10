34705923f50add7289321bba9761b3d485a7f346
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-10 16:39:40 -0300

    feat(domain): add Phone value object

---
+++ KluskaStore.Domain/ValueObjects/Phone.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace KluskaStore.Domain.ValueObjects;
+
+public partial class Phone : ValueObject<string> {
+  public Phone(string value) : base(value) {
+    if (!PhoneRegex().IsMatch(value)) throw new ArgumentException("Invalid Phone.", nameof(value));
+  }
+  
+  [GeneratedRegex(@"\+?\d{12,15}")]
+  private static partial Regex PhoneRegex();
+}
