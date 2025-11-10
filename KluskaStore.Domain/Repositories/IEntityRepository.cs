666820c2bbf8ffa51752cd085e082df259c9118d
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-10 17:47:09 -0300

    fix(repository): make update and delete sync methods in interface

--- KluskaStore.Domain/Repositories/IEntityRepository.cs
+++ KluskaStore.Domain/Repositories/IEntityRepository.cs
@@ -6,6 +6,6 @@ public interface IEntityRepository<T, in TId> where T : class, IEntity {
   Task<T?> GetByIdAsync(TId id);
   Task<IEnumerable<T>> GetAllAsync();
   Task AddAsync(T entity);
-  Task UpdateAsync(T entity);
-  Task DeleteAsync(T entity);
+  void UpdateAsync(T entity);
+  void DeleteAsync(T entity);
 }
