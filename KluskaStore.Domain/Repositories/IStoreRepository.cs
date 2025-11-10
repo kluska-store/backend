897b864b4d3280224f2771a1bd665eff0b085be6
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-10 17:27:05 -0300

    feat(repository): add store repository

---
+++ KluskaStore.Domain/Repositories/IStoreRepository.cs
@@ -0,0 +1,8 @@
+﻿using KluskaStore.Domain.Entities;
+using KluskaStore.Domain.ValueObjects;
+
+namespace KluskaStore.Domain.Repositories;
+
+public interface IStoreRepository : IEntityRepository<Store, Guid> {
+  Task<Store?> GetByCnpjAsync(Cnpj cnpj);
+}
