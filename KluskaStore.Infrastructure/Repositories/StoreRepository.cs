12aa513ed887b55a38cced80c4c3462cbc3f3304
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-10 17:50:16 -0300

    feat(repository): implement store repository

---
+++ KluskaStore.Infrastructure/Repositories/StoreRepository.cs
@@ -0,0 +1,10 @@
+﻿using KluskaStore.Domain.Entities;
+using KluskaStore.Domain.Repositories;
+using KluskaStore.Domain.ValueObjects;
+using KluskaStore.Infrastructure.Data;
+
+namespace KluskaStore.Infrastructure.Repositories;
+
+public class StoreRepository(AppDbContext context) : EntityRepository<Store, Guid>(context), IStoreRepository {
+  public async Task<Store?> GetByCnpjAsync(Cnpj cnpj) => await Context.Stores.FindAsync(cnpj);
+}
