370cabf987d3b353d16f71f232d157dd19c49bb0
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-10 14:37:43 -0300

    feat(domain): add Email value object

---
+++ KluskaStore.Domain/ValueObjects/Email.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace KluskaStore.Domain.ValueObjects;
+
+public partial class Email : ValueObject<string> {
+  public Email(string value) : base(value) {
+    if (!EmailRegex().IsMatch(value)) throw new ArgumentException("Invalid email.", nameof(value));
+  }
+
+  [GeneratedRegex(@"\w+(\.\w+)*@\w+(\.\w+)+", RegexOptions.Compiled)]
+  private static partial Regex EmailRegex();
+}
