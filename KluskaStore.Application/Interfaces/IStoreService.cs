c9ee4b87b6bd435df4aad7836052752e12a63618
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-11 14:11:28 -0300

    feat(service): add interfaces, abstract classes for services

---
+++ KluskaStore.Application/Interfaces/IStoreService.cs
@@ -0,0 +1,9 @@
+﻿using KluskaStore.Application.DTOs;
+using KluskaStore.Application.DTOs.Store;
+
+namespace KluskaStore.Application.Interfaces;
+
+public interface IStoreService : IService {
+  Task<StoreResponse> GetStoreByIdAsync(GuidRequest request);
+  Task<StoreResponse> CreateStoreAsync(CreateStoreRequest request);
+}
---
+++ KluskaStore.Application/Services/StoreService.cs
@@ -0,0 +1,26 @@
+﻿using KluskaStore.Application.DTOs;
+using KluskaStore.Application.DTOs.Store;
+using KluskaStore.Application.Interfaces;
+using KluskaStore.Domain.Repositories;
+
+namespace KluskaStore.Application.Services;
+
+public class StoreService : Service, IStoreService {
+  private readonly ICreateStoreUseCase _createStoreUseCase;
+  private readonly IGetStoreByIdUseCase _getStoreByIdUseCase;
+
+  public StoreService(
+    IUnitOfWork uow,
+    ICreateStoreUseCase createStoreUseCase,
+    IGetStoreByIdUseCase getStoreByIdUseCase
+  ) : base(uow) {
+    _createStoreUseCase = createStoreUseCase;
+    _getStoreByIdUseCase = getStoreByIdUseCase;
+  }
+
+  public async Task<StoreResponse> GetStoreByIdAsync(GuidRequest request) =>
+    await _getStoreByIdUseCase.ExecuteAsync(request);
+
+  public async Task<StoreResponse> CreateStoreAsync(CreateStoreRequest request) =>
+    await _createStoreUseCase.ExecuteAsync(request);
+}
