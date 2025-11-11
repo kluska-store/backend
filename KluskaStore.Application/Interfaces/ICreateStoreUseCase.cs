4eecf6419e7c7206ab65ba77383d138eeec6a0f5
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-10 21:24:47 -0300

    feat(use case): add create store use case

---
+++ KluskaStore.Application/Interfaces/ICreateStoreUseCase.cs
@@ -0,0 +1,5 @@
+﻿using KluskaStore.Application.DTOs.Store;
+
+namespace KluskaStore.Application.Interfaces;
+
+public interface ICreateStoreUseCase : IUseCase<CreateStoreRequest, StoreResponse> { }
---
+++ KluskaStore.Application/UseCases/Store/CreateStoreUseCase.cs
@@ -0,0 +1,22 @@
+﻿using KluskaStore.Application.DTOs.Store;
+using KluskaStore.Application.Interfaces;
+using KluskaStore.Domain.Repositories;
+using KluskaStore.Domain.ValueObjects;
+
+namespace KluskaStore.Application.UseCases.Store;
+
+public class CreateStoreUseCase(IUnitOfWork uow)
+  : UseCase<CreateStoreRequest, StoreResponse>(uow), ICreateStoreUseCase {
+  
+  public override async Task<StoreResponse> ExecuteAsync(CreateStoreRequest request) {
+    var cnpj = new Cnpj(request.Cnpj);
+    var email = new Email(request.Email);
+
+    var store = new Domain.Entities.Store(cnpj, request.Name, email, request.Password);
+
+    await UoW.Stores.AddAsync(store);
+    await UoW.SaveChangesAsync();
+
+    return new StoreResponse(store.Id, store.Cnpj.Value, store.Name, store.Email.Value, store.IsActive);
+  }
+}
