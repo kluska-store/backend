f4db74ae88dce38cc1bba360b9c7da605dc223e0
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-12 08:50:10 -0300

    feat(test): add unit test for CNPJ

---
+++ KluskaStore.Tests/Domain/ValueObjects/CnpjTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using KluskaStore.Domain.ValueObjects;
+
+namespace KluskaStore.Tests.Domain.ValueObjects;
+
+public class CnpjTests {
+  [Theory]
+  [InlineData("04658924000130")]
+  [InlineData("10619284000152")]
+  public void GivenValidCnpj_WhenNotSkippingVerifierDigitsValidation_ThenCreatesVo(string cnpj) {
+    var result = Cnpj.New(cnpj);
+
+    result.IsValid.Should().BeTrue();
+    result.Error.Should().BeNull();
+    result.Vo.Should().BeAssignableTo<Cnpj>();
+    result.Vo.Value.Should().Be(cnpj);
+  }
+
+  [Theory]
+  [InlineData("0465892400013000")]
+  [InlineData("04658924000132")]
+  [InlineData("04658924w00013")]
+  [InlineData("00000000000000")]
+  [InlineData("04658924000")]
+  public void GivenInvalidCnpj_WhenNotSkippingVerifierDigistValidation_ThenReturnsInvalidVoResult(string cnpj) {
+    var result = Cnpj.New(cnpj);
+
+    result.IsValid.Should().BeFalse();
+    result.Error.Should().NotBeNullOrWhiteSpace();
+    result.Vo.Should().BeNull();
+  }
+}
