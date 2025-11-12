7524e52b52d8d804c2c7417c1b182d3db0f50034
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-12 14:21:54 -0300

    feat(tests): add unit test for Email

---
+++ KluskaStore.Tests/Domain/ValueObjects/EmailTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using KluskaStore.Domain.ValueObjects;
+
+namespace KluskaStore.Tests.Domain.ValueObjects;
+
+public class EmailTests {
+  [Theory]
+  [InlineData("example@email.com")]
+  [InlineData("example@email.com.br")]
+  [InlineData("bigger.example@email.com")]
+  [InlineData("numeric55.example_123@email.com")]
+  public void GivenValidEmail_ThenCreatesVo(string email) {
+    var result = Email.New(email);
+
+    result.IsValid.Should().BeTrue();
+    result.Error.Should().BeNull();
+    result.Vo.Should().BeAssignableTo<Email>();
+    result.Vo.Value.Should().Be(email);
+  }
+
+  [Theory]
+  [InlineData("example@.com")]
+  [InlineData("example@email")]
+  [InlineData(".example@email.com")]
+  [InlineData("example@email.com.")]
+  [InlineData("exampleemail.com")]
+  [InlineData("example@email@com")]
+  public void GivenInvalidEmail_ThenCreatesInvalidVoResult(string email) {
+    var result = Email.New(email);
+
+    result.IsValid.Should().BeFalse();
+    result.Error.Should().NotBeNullOrWhiteSpace();
+    result.Vo.Should().BeNull();
+  }
+}
