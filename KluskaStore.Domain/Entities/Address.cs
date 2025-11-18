cc8c3d1dc7f61dc569df012b7f6cb361c6faa37f
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-18 12:13:33 -0300

    feat(address): add Address domain entity

---
+++ KluskaStore.Domain/Entities/Address.cs
@@ -0,0 +1,33 @@
+﻿using KluskaStore.Domain.ValueObjects;
+
+namespace KluskaStore.Domain.Entities;
+
+public class Address : Entity<uint>
+{
+    private Address() { }
+
+    private Address(
+        string country,
+        string state,
+        string city,
+        string street,
+        uint number,
+        PostalCode postalCode,
+        string? complement
+    )
+    {
+        Country = country;
+        State = state;
+        City = city;
+        Street = street;
+        Number = number;
+        PostalCode = postalCode;
+    }
+    
+    public string Country { get; private set; }
+    public string State { get; private set; }
+    public string City { get; private set; }
+    public string Street { get; private set; }
+    public uint Number { get; private set; }
+    public PostalCode PostalCode { get; set; }
+}
