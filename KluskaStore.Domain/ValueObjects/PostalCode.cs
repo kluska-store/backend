b5180eb62b89bc58bc6490f11352e5d927eb64ab
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-18 12:11:44 -0300

    feat(address): add PostalCode VO

---
+++ KluskaStore.Domain/ValueObjects/PostalCode.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+using KluskaStore.Domain.Shared;
+
+namespace KluskaStore.Domain.ValueObjects;
+
+public partial record PostalCode : ValueObject<string>
+{
+    private PostalCode(string value) : base(value) { }
+    
+    public static Result<PostalCode> Create(string value) =>
+        PostalCodeRegex().IsMatch(value)
+            ? Result<PostalCode>.Success(new PostalCode(value))
+            : Result<PostalCode>.Failure("Invalid postal code");
+
+    [GeneratedRegex(@"\d{8}")]
+    private static partial Regex PostalCodeRegex();
+}
