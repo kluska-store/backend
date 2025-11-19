835cb158c24be13bf401e7a1d69563d944644142
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-18 23:44:01 -0300

    feat(mapper): add and adapt mappers for address and store

---
+++ KluskaStore.Application/Mappers/AddressMapper.cs
@@ -0,0 +1,11 @@
+﻿using KluskaStore.Application.Features.Addresses;
+using KluskaStore.Domain.Entities;
+using Riok.Mapperly.Abstractions;
+
+namespace KluskaStore.Application.Mappers;
+
+[Mapper]
+public static partial class AddressMapper
+{
+    public static partial AddressResponse ToResponse(this Address address);
+}
--- KluskaStore.Application/Mappers/StoreMapper.cs
+++ KluskaStore.Application/Mappers/StoreMapper.cs
@@ -1,4 +1,5 @@
-﻿using KluskaStore.Application.Features.Stores;
+﻿using KluskaStore.Application.Features.Addresses;
+using KluskaStore.Application.Features.Stores;
 using KluskaStore.Domain.Entities;
 using Riok.Mapperly.Abstractions;
 
@@ -8,4 +9,6 @@ namespace KluskaStore.Application.Mappers;
 public static partial class StoreMapper
 {
     public static partial StoreResponse ToResponse(this Store s);
+
+    private static AddressResponse MapAddressToResponse(Address address) => address.ToResponse();
 }
