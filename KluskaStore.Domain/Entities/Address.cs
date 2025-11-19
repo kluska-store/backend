f3f611b9478f97bc6b77f175d4b4a92b118b682e
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-18 23:39:39 -0300

    feat(address): add Create factory method

--- KluskaStore.Domain/Entities/Address.cs
+++ KluskaStore.Domain/Entities/Address.cs
@@ -1,4 +1,5 @@
-﻿using KluskaStore.Domain.ValueObjects;
+﻿using KluskaStore.Domain.Shared;
+using KluskaStore.Domain.ValueObjects;
 
 namespace KluskaStore.Domain.Entities;
 
@@ -24,7 +25,7 @@ public class Address : Entity<uint>
         PostalCode = postalCode;
         Complement = complement;
     }
-    
+
     public string Country { get; private set; }
     public string State { get; private set; }
     public string City { get; private set; }
@@ -32,4 +33,25 @@ public class Address : Entity<uint>
     public uint Number { get; private set; }
     public PostalCode PostalCode { get; set; }
     public string? Complement;
+
+    public static Result<Address> Create(
+        string country,
+        string state,
+        string city,
+        string street,
+        uint number,
+        PostalCode postalCode,
+        string? complement
+    )
+    {
+        List<string> errors = [];
+        if (string.IsNullOrWhiteSpace(country)) errors.Add("Country must not be null");
+        if (string.IsNullOrWhiteSpace(state)) errors.Add("State must not be null");
+        if (string.IsNullOrWhiteSpace(city)) errors.Add("City must not be null");
+        if (string.IsNullOrWhiteSpace(street)) errors.Add("Street must not be null");
+
+        return errors.Count > 0 
+            ? Result<Address>.Failure(errors) 
+            : Result<Address>.Success(new Address(country, state, city, street, number, postalCode, complement));
+    }
 }
