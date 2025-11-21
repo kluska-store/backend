9633e465fb79128505cbd672dbddbb33e3001190
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-21 03:23:58 -0300

    feat(domain): add Session entity and its related VOs

---
+++ KluskaStore.Domain/Entities/Session.cs
@@ -0,0 +1,24 @@
+﻿using KluskaStore.Domain.Shared;
+using KluskaStore.Domain.ValueObjects;
+
+namespace KluskaStore.Domain.Entities;
+
+public class Session : Entity<string>
+{
+    private Session(SessionOwner owner, DateTime createdAt)
+    {
+        Owner = owner;
+        CreatedAt = createdAt;
+    }
+
+    public SessionOwner Owner { get; protected set; }
+    public DateTime CreatedAt { get; protected set; }
+    public DateTime ExpiresAt => CreatedAt.AddMonths(3);
+
+    public bool IsExpired() => DateTime.UtcNow > ExpiresAt;
+
+    private static Result<Session> Create(Result<SessionOwner> ownerResult, DateTime createdAt) =>
+        ownerResult.IsFailure
+            ? Result<Session>.Failure(ownerResult.Errors)
+            : Result<Session>.Success(new Session(ownerResult.Value, createdAt));
+}
---
+++ KluskaStore.Domain/ValueObjects/SessionOwner.cs
@@ -0,0 +1,30 @@
+﻿using KluskaStore.Domain.Shared;
+
+namespace KluskaStore.Domain.ValueObjects;
+
+public class SessionOwner
+{
+    public enum OwnerTypeEnum
+    {
+        User = 1,
+        Store = 2
+    }
+
+    private SessionOwner(OwnerTypeEnum type, Guid ownerId)
+    {
+        OwnerType = type;
+        OwnerId = ownerId;
+    }
+
+    public OwnerTypeEnum OwnerType { get; }
+    public Guid OwnerId { get; }
+
+    private static Result<SessionOwner> Create(OwnerTypeEnum type, Guid ownerId) =>
+        ownerId == Guid.Empty
+            ? Result<SessionOwner>.Failure("Invalid session owner id")
+            : Result<SessionOwner>.Success(new SessionOwner(type, ownerId));
+
+    public static Result<SessionOwner> User(Guid userId) => Create(OwnerTypeEnum.User, userId);
+
+    public static Result<SessionOwner> Store(Guid storeId) => Create(OwnerTypeEnum.Store, storeId);
+}
