bc50e3ae3362e07cedb87b275ecbff6b8a28a340
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-21 02:19:26 -0300

    fix(DDD): transform Address from entity to VO to fix DDD

--- KluskaStore.Domain/Repositories/IAddressRepository.cs
+++
@@ -1,5 +0,0 @@
-﻿using KluskaStore.Domain.Entities;
-
-namespace KluskaStore.Domain.Repositories;
-
-public interface IAddressRepository : IEntityRepository<Address, uint>;
---
+++ KluskaStore.Domain/ValueObjects/Address.cs
@@ -0,0 +1,54 @@
+﻿using KluskaStore.Domain.Shared;
+
+namespace KluskaStore.Domain.ValueObjects;
+
+public class Address
+{
+    private Address(
+        string country,
+        string state,
+        string city,
+        string street,
+        uint number,
+        PostalCode postalCode,
+        string? complement
+    )
+    {
+        Country = country;
+        State = state;
+        City = city;
+        Street = street;
+        Number = number;
+        PostalCode = postalCode;
+        Complement = complement;
+    }
+
+    public string Country { get;  }
+    public string State { get;  }
+    public string City { get;  }
+    public string Street { get;  }
+    public uint Number { get;  }
+    public PostalCode PostalCode { get;  }
+    public string? Complement { get; }
+
+    public static Result<Address> Create(
+        string country,
+        string state,
+        string city,
+        string street,
+        uint number,
+        PostalCode postalCode,
+        string? complement
+    )
+    {
+        List<string> errors = [];
+        if (string.IsNullOrWhiteSpace(country)) errors.Add("Country must not be null");
+        if (string.IsNullOrWhiteSpace(state)) errors.Add("State must not be null");
+        if (string.IsNullOrWhiteSpace(city)) errors.Add("City must not be null");
+        if (string.IsNullOrWhiteSpace(street)) errors.Add("Street must not be null");
+
+        return errors.Count > 0 
+            ? Result<Address>.Failure(errors) 
+            : Result<Address>.Success(new Address(country, state, city, street, number, postalCode, complement));
+    }
+}
