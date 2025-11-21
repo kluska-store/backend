b3bc4ff81ac75857dc363a513cd9a7ce57ee48a2
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-21 13:52:57 -0300

    feat(domain): add User entity and its related VOs

---
+++ KluskaStore.Domain/Entities/User.cs
@@ -0,0 +1,96 @@
+﻿using KluskaStore.Domain.Shared;
+using KluskaStore.Domain.ValueObjects;
+
+namespace KluskaStore.Domain.Entities;
+
+public class User : Entity<Guid>
+{
+    protected User() { }
+
+    private User(
+        Cpf cpf,
+        Email email,
+        string username,
+        bool isActive,
+        Phone phone,
+        DateOnly birthday,
+        string passwordHash
+    )
+    {
+        Cpf = cpf;
+        Email = email;
+        Username = username;
+        IsActive = isActive;
+        Phone = phone;
+        Birthday = birthday;
+        PasswordHash = passwordHash;
+    }
+
+    public Cpf Cpf { get; protected set; }
+    public Email Email { get; protected set; }
+    public string Username { get; protected set; }
+    public string? ProfilePicture { get; protected set; }
+    public bool IsActive { get; protected set; }
+    public Phone Phone { get; protected set; }
+    public DateOnly Birthday { get; protected set; }
+    public string PasswordHash { get; protected set; }
+
+    public static Result<User> Create(
+        Cpf cpf,
+        Email email,
+        string username,
+        bool isActive,
+        Phone phone,
+        DateOnly birthday,
+        string passwordHash
+    )
+    {
+        List<string> errors = [];
+        if (string.IsNullOrWhiteSpace(username)) errors.Add("Username must not be empty");
+        if (string.IsNullOrWhiteSpace(passwordHash)) errors.Add("Password must not be empty");
+        if (birthday.AddYears(18) > DateOnly.FromDateTime(DateTime.UtcNow))
+        {
+            errors.Add("User must be 18 or more years old");
+        }
+
+        if (errors.Count > 0) return Result<User>.Failure(errors);
+
+        var user = new User(cpf, email, username, isActive, phone, birthday, passwordHash);
+        return Result<User>.Success(user);
+    }
+
+    public void ChangeEmail(Email newEmail) => Email = newEmail;
+
+    public Result<User> ChangeUsername(string newUsername)
+    {
+        if (string.IsNullOrWhiteSpace(newUsername)) return Result<User>.Failure("Username must not be empty");
+
+        Username = newUsername;
+        return Result<User>.Success(this);
+    }
+
+    public void Activate() => IsActive = true;
+
+    public void Deactivate() => IsActive = false;
+
+    public void ChangePhone(Phone newPhone) => Phone = newPhone;
+
+    public Result<User> ChangeBirthday(DateOnly newBirthday)
+    {
+        if (newBirthday.AddYears(18) > DateOnly.FromDateTime(DateTime.UtcNow))
+        {
+            return Result<User>.Failure("User must be 18 or more years old");
+        }
+
+        Birthday = newBirthday;
+        return Result<User>.Success(this);
+    }
+
+    public Result<User> ChangePassword(string newPasswordHash)
+    {
+        if (string.IsNullOrWhiteSpace(newPasswordHash)) return Result<User>.Failure("Password must not be empty");
+
+        PasswordHash = newPasswordHash;
+        return Result<User>.Success(this);
+    }
+}
---
+++ KluskaStore.Domain/ValueObjects/Cpf.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+using KluskaStore.Domain.Interfaces;
+using KluskaStore.Domain.Shared;
+
+namespace KluskaStore.Domain.ValueObjects;
+
+public partial class Cpf : IValueObject
+{
+    private Cpf(string value) => Value = value;
+
+    public string Value { get; }
+
+    [GeneratedRegex(@"\d{11}")]
+    private static partial Regex CpfRegex();
+
+    private static bool Validate(string cpf, bool skipCheckDigits)
+    {
+        if (!CpfRegex().IsMatch(cpf)) return false;
+        if (skipCheckDigits) return true;
+        if (cpf.Distinct().Count() == 1) return false;
+
+        var sum = 0;
+        List<int> weights = [10, 9, 8, 7, 6, 5, 4, 3, 2];
+
+        for (var i = 0; i < 9; i++) sum += weights[i] * (int)char.GetNumericValue(cpf[i]);
+        sum %= 11;
+        var verifierDigit = sum is 1 or 0 ? 0 : 11 - sum;
+
+        if (verifierDigit != (int)char.GetNumericValue(cpf[9])) return false;
+
+        sum = 0;
+        weights.Insert(0, 11);
+
+        for (var i = 0; i < 10; i++) sum += weights[i] * (int)char.GetNumericValue(cpf[i]);
+        sum %= 11;
+        verifierDigit = sum is 1 or 0 ? 0 : 11 - sum;
+
+        return verifierDigit == (int)char.GetNumericValue(cpf[10]);
+    }
+
+    public static Result<Cpf> Create(string value, bool skipVerifierDigitsValidation = false) =>
+        Validate(value, skipVerifierDigitsValidation)
+            ? Result<Cpf>.Success(new Cpf(value))
+            : Result<Cpf>.Failure("Invalid cpf");
+}
