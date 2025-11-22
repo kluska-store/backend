b042f76cda4f396f0e57721c595948e55faeeabb
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-11-22 02:14:26 -0300

    feat(phone): improve phone VO to better divide all informations contained in a phone number

--- KluskaStore.Domain/ValueObjects/Phone.cs
+++ KluskaStore.Domain/ValueObjects/Phone.cs
@@ -6,15 +6,21 @@ namespace KluskaStore.Domain.ValueObjects;
 
 public partial class Phone : IValueObject
 {
-    private Phone(string value) => Value = value;
+    private Phone(string fullPhone) => FullPhone = fullPhone;
 
-    public string Value { get; }
+    public string FullPhone { get; }
+    public string Ddi => GetPhonePart("ddi");
+    public string Ddd => GetPhonePart("ddd");
+    public string PhoneNumber => GetPhonePart("phone");
+
 
     public static Result<Phone> Create(string value) =>
         PhoneRegex().IsMatch(value)
             ? Result<Phone>.Success(new Phone(value))
             : Result<Phone>.Failure("Invalid phone");
 
-    [GeneratedRegex(@"^\+?\d{12,15}$")]
+    private string GetPhonePart(string partName) => PhoneRegex().Match(FullPhone).Groups[partName].Value;
+
+    [GeneratedRegex(@"^\+(?<ddi>\d{1,3}) \((?<ddd>\d{1,3})\) (?<phone>\d{4,5}-\d{4})$")]
     private static partial Regex PhoneRegex();
 }
