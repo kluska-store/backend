d0688c0b4bd448d13604aea03109f9868add7b11
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-12-01 16:08:10 -0300

    feat(domain): add Item entity

---
+++ KluskaStore.Domain/Entities/Item.cs
@@ -0,0 +1,28 @@
+﻿using KluskaStore.Domain.Shared;
+
+namespace KluskaStore.Domain.Entities;
+
+public class Item : Entity<uint>
+{
+    public Product Product { get; private set; }
+    public uint Quantity { get; private set; }
+
+    private Item() { }
+
+    internal Item(Product product, uint quantity)
+    {
+        Product = product;
+        Quantity = quantity;
+    }
+
+    public static Result<Item> Create(Product product, uint quantity) =>
+        quantity == 0
+            ? Result<Item>.Failure("Items must be created with quantity >= 1")
+            : Result<Item>.Success(new Item(product, quantity));
+
+    public bool ChangeQuantity(uint newQuantity)
+    {
+        Quantity = newQuantity;
+        return Quantity > 0;
+    }
+}
---
+++ KluskaStore.Tests/Domain/Entities/ItemTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using KluskaStore.Domain.Entities;
+
+namespace KluskaStore.Tests.Domain.Entities;
+
+public class ItemTests
+{
+    private readonly Item _sut = new(
+        new Product(
+            new Dictionary<string, string>(),
+            10,
+            "product"
+        ),
+        2
+    );
+
+    [Fact]
+    public void GivenEntityCreation_WhenInitialDataIsValid_ThenCreatesItem()
+    {
+        var result = Item.Create(_sut.Product, _sut.Quantity);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Errors.Should().BeEmpty();
+        result.Value.Should().BeAssignableTo<Item>();
+        result.Value.Product.Should().Be(_sut.Product);
+        result.Value.Quantity.Should().Be(_sut.Quantity);
+    }
+
+    [Fact]
+    public void GivenEntityCreation_WhenInitialDataIsInvalid_ThenReturnsFailure()
+    {
+        var result = Item.Create(null!, 0);
+
+        result.IsFailure.Should().BeTrue();
+        result.Errors.Should().NotBeNullOrEmpty();
+        result.Value.Should().BeNull();
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(20)]
+    [InlineData(1)]
+    public void GivenQuantityChange_ThenChangesQuantityAndReturnsItemValidity(uint amount)
+    {
+        _sut.ChangeQuantity(amount).Should().Be(_sut.Quantity != 0);
+        _sut.Quantity.Should().Be(amount);
+    }
+}
