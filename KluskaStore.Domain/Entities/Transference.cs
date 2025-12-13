77b1a19ab454fd8f06b79f03294f3824d14aa7c5
lucasdonini <lkd.lucas@gmail.com>
2025-12-13 01:36:10 -0300

    feat(domain): add Transference entity

---
+++ KluskaStore.Domain/Entities/Transference.cs
@@ -0,0 +1,30 @@
+﻿using KluskaStore.Domain.Entities.Generics;
+using KluskaStore.Domain.Shared;
+
+namespace KluskaStore.Domain.Entities;
+
+public class Transference : DefaultIdentityEntity
+{
+    public Guid ReceiverStoreId { get; private set; }
+    public decimal Value { get; private set; }
+
+
+    private Transference() { }
+
+    internal Transference(Guid receiverStoreId, decimal value)
+    {
+        ReceiverStoreId = receiverStoreId;
+        Value = value;
+    }
+
+    public static Result<Transference> Create(Guid receiverStoreId, decimal value)
+    {
+        List<string> errors = [];
+        if (receiverStoreId == Guid.Empty) errors.Add("Receiver Store Id must not be empty");
+        if (value <= 0) errors.Add("Value must be grater than 0");
+
+        return errors.Count > 0
+            ? Result<Transference>.Failure(errors)
+            : Result<Transference>.Success(new Transference(receiverStoreId, value));
+    }
+}
