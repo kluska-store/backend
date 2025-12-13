f3d29884dc937e9cefc865d08841209a1a3e5c8a
lucasdonini <lkd.lucas@gmail.com>
2025-12-12 21:11:48 -0300

    feat(VO): add OrderItem value object

---
+++ KluskaStore.Domain/ValueObjects/OrderItem.cs
@@ -0,0 +1,56 @@
+﻿using KluskaStore.Domain.Interfaces;
+using KluskaStore.Domain.Shared;
+
+namespace KluskaStore.Domain.ValueObjects;
+
+public record OrderItem : IValueObject
+{
+    private readonly Dictionary<string, string> _specifications;
+
+    public Guid ProductId { get; }
+    public string Name { get; }
+    public decimal UnitPrice { get; }
+    public string? Description { get; }
+    public uint Quantity { get; }
+    public decimal TotalPrice => UnitPrice * Quantity;
+    public Dictionary<string, string> Specifications => _specifications;
+
+    internal OrderItem(
+        Guid productId,
+        string name,
+        decimal unitPrice,
+        string? description,
+        uint quantity,
+        IEnumerable<KeyValuePair<string, string>> specifications
+    )
+    {
+        ProductId = productId;
+        Name = name;
+        UnitPrice = unitPrice;
+        Description = description;
+        Quantity = quantity;
+        _specifications = specifications.ToDictionary();
+    }
+
+    public static Result<OrderItem> Create(
+        Guid productId,
+        string name,
+        decimal unitPrice,
+        string? description,
+        uint quantity,
+        IEnumerable<KeyValuePair<string, string>> specifications
+    )
+    {
+        List<string> errors = [];
+        if (productId == Guid.Empty) errors.Add("Product Id must not be empty");
+        if (string.IsNullOrWhiteSpace(name)) errors.Add("Name must not be empty");
+        if (unitPrice <= 0) errors.Add("Unit Price must be grater than 0");
+        if (quantity <= 0) errors.Add("Quantity must be grater than 0");
+
+        return errors.Count > 0
+            ? Result<OrderItem>.Failure(errors)
+            : Result<OrderItem>.Success(
+                new OrderItem(productId, name, unitPrice, description, quantity, specifications)
+            );
+    }
+}
---
+++ KluskaStore.Tests/Domain/ValueObjects/OrderItemTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using KluskaStore.Domain.ValueObjects;
+
+namespace KluskaStore.Tests.Domain.ValueObjects;
+
+public class OrderItemTests
+{
+    [Fact]
+    public void GivenValueObjectCreation_WhenDataIsValid_ThenCreatesOrderItem()
+    {
+        var productId = Guid.NewGuid();
+        var name = "name";
+        var unitPrice = 10m;
+        var description = "description";
+        var quantity = 2u;
+        var specifications = new Dictionary<string, string>() { ["size"] = "10cm x 10cm" };
+        var result = OrderItem.Create(productId, name, unitPrice, description, quantity, specifications);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Errors.Should().BeEmpty();
+        result.Value.Should().BeAssignableTo<OrderItem>();
+
+        var orderItem = result.Value;
+        orderItem.ProductId.Should().Be(productId);
+        orderItem.Name.Should().Be(name);
+        orderItem.UnitPrice.Should().Be(unitPrice);
+        orderItem.Description.Should().Be(description);
+        orderItem.Quantity.Should().Be(quantity);
+        orderItem.Specifications.Should().BeEquivalentTo(specifications);
+        orderItem.TotalPrice.Should().Be(unitPrice * quantity);
+    }
+
+    [Fact]
+    public void GivenValueObjectCreation_WhenDataIsInvalid_ThenReturnsFailure()
+    {
+        var result = OrderItem.Create(Guid.Empty, null!, -10, null, 0, null!);
+
+        result.IsFailure.Should().BeTrue();
+        result.Errors.Should().NotBeNullOrEmpty();
+        result.Value.Should().BeNull();
+    }
+}
