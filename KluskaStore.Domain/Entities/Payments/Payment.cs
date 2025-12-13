38a004b0aa92479fbcb03ede05f7bb501dd6317a
lucasdonini <lkd.lucas@gmail.com>
2025-12-13 02:16:03 -0300

    feat(domain): add Payment entity

---
+++ KluskaStore.Domain/Entities/Payments/Payment.cs
@@ -0,0 +1,40 @@
+﻿using KluskaStore.Domain.Entities.Generics;
+using KluskaStore.Domain.Shared;
+
+namespace KluskaStore.Domain.Entities.Payments;
+
+public class Payment : DefaultIdentityEntity
+{
+    private readonly List<Transference> _transferences;
+
+    public Guid PayerUserId { get; private set; }
+    public DateTime Date { get; private set; }
+    public Guid OrderId { get; private set; }
+
+    public IReadOnlyList<Transference> Transferences => _transferences.AsReadOnly();
+    public decimal Value => _transferences.Select(t => t.Value).Sum();
+
+    private Payment() { }
+
+    internal Payment(Guid payerUserId, DateTime date, Guid orderId, IEnumerable<Transference> transfererences)
+    {
+        PayerUserId = payerUserId;
+        Date = date;
+        OrderId = orderId;
+        _transferences = transfererences.ToList();
+    }
+
+    public static Result<Payment> Create(Guid payerUserId, DateTime date, Guid orderId, IEnumerable<Transference> transferences)
+    {
+        var innerTransferences = transferences.ToList();
+        List<string> errors = [];
+        if (payerUserId == Guid.Empty) errors.Add("Payer User Id must not be empty");
+        if (date > DateTime.UtcNow) errors.Add("Payment Date must not be in the future");
+        if (orderId == Guid.Empty) errors.Add("Order Id must not be empty");
+        if (innerTransferences.Count < 1) errors.Add("A Payment must contain at least 1 Transference");
+
+        return errors.Count > 0
+            ? Result<Payment>.Failure(errors)
+            : Result<Payment>.Success(new Payment(payerUserId, date, orderId, innerTransferences));
+    }
+}
---
+++ KluskaStore.Tests/Domain/Entities/Payments/PaymentTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using KluskaStore.Domain.Entities.Payments;
+
+namespace KluskaStore.Tests.Domain.Entities.Payments;
+
+public class PaymentTests
+{
+    [Fact]
+    public void GivenEntityCreation_WhenInitialDataIsValid_ThenCreatesPayment()
+    {
+        var userId = Guid.NewGuid();
+        var orderId = Guid.NewGuid();
+        var date = DateTime.UtcNow.AddDays(-30);
+        List<Transference> transferences = [new(Guid.NewGuid(), 10)];
+        var result = Payment.Create(userId, date, orderId, transferences);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Errors.Should().BeEmpty();
+        result.Value.Should().BeAssignableTo<Payment>();
+
+        var payment = result.Value;
+        payment.PayerUserId.Should().Be(userId);
+        payment.OrderId.Should().Be(orderId);
+        payment.Date.Should().Be(date);
+        payment.Transferences.Should().BeEquivalentTo(transferences);
+        payment.Value.Should().Be(transferences.Select(t => t.Value).Sum());
+    }
+
+    [Fact]
+    public void GivenEntityCreation_WhenInitialDataIsInvalid_ThenReturnsFailure()
+    {
+        var result = Payment.Create(Guid.Empty, DateTime.UtcNow.AddDays(1), Guid.Empty, new List<Transference>());
+
+        result.IsFailure.Should().BeTrue();
+        result.Errors.Count.Should().Be(4);
+        result.Value.Should().BeNull();
+    }
+}
