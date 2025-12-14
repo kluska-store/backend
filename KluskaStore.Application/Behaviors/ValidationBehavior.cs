9bf86d72fb781ead77787ee31decaa226a523ced
lucasdonini <lkd.lucas@gmail.com>
2025-12-14 00:04:06 -0300

    feat(application): add CreateUser validator

---
+++ KluskaStore.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using ValidationException = FluentValidation.ValidationException;
+
+namespace KluskaStore.Application.Behaviors;
+
+public sealed class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators)
+    : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+{
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken
+    )
+    {
+        if (validators.Any())
+        {
+            var context = new ValidationContext<TRequest>(request);
+
+            var failures = validators
+                .Select(validator => validator.Validate(context))
+                .SelectMany(result => result.Errors)
+                .Where(failure => failure is not null)
+                .ToList();
+
+            if (failures.Count > 0) throw new ValidationException(failures);
+        }
+
+        return await next(cancellationToken);
+    }
+}
---
+++ KluskaStore.Application/Features/Users/CreateUser/CreateUserValidator.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using FluentValidation;
+
+namespace KluskaStore.Application.Features.Users.CreateUser;
+
+public class CreateUserValidator : AbstractValidator<CreateUserCommand>
+{
+    public CreateUserValidator()
+    {
+        RuleFor(c => c.Email).NotEmpty().EmailAddress();
+        RuleFor(c => c.Cpf).NotEmpty().Length(11);
+        RuleFor(c => c.Username).NotEmpty();
+        RuleFor(c => c.Phone).NotEmpty();
+        RuleFor(c => c.Birthday).LessThanOrEqualTo(DateOnly.FromDateTime(DateTime.UtcNow));
+        RuleFor(c => c.RawPassword).NotEmpty();
+    }
+}
