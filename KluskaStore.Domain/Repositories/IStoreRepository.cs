2c7c694ed7b7821c4936b223cae4cd477855dd98
lucasdonini <lkd.lucas@gmail.com>
2025-12-13 22:37:40 -0300

    fix(domain): remove Repositories from Domain (wrong layer)

--- KluskaStore.Domain/Repositories/IStoreRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using KluskaStore.Domain.Entities.Accounts;
-
-namespace KluskaStore.Domain.Repositories;
-
-public interface IStoreRepository
-{
-    Task<Store?> GetByIdAsync(Guid id);
-    Task AddAsync(Store store);
-}
