db35baea107709f79a4d849e7175dc69c5ccde01
lucasdonini <lkd.lucas@gmail.com>
2025-12-14 00:46:38 -0300

    feat(application): add Dependency Injection class

---
+++ KluskaStore.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using KluskaStore.Application.Behaviors;
+
+namespace KluskaStore.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var thisAssembly = typeof(DependencyInjection).Assembly;
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(thisAssembly));
+        services.AddValidatorsFromAssembly(thisAssembly);
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        return services;
+    }
+}
