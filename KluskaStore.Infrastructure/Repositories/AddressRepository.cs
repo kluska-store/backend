a38e10246123ef11f18eb51d17afbe85bfaeeb2e
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-01-28 14:49:48 -0300

    restart(infrastructure): recreates an empty Infrastructure project

--- KluskaStore.Infrastructure/Repositories/AddressRepository.cs
+++
@@ -1,5 +0,0 @@
-﻿using KluskaStore.Infrastructure.Data;
-
-namespace KluskaStore.Infrastructure.Repositories;
-
-public class AddressRepository(AppDbContext context) : EntityRepository<Address, uint>(context), IAddressRepository;
