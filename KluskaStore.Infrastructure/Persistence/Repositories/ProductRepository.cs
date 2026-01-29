0043bcab17b4b6915eb4a833500cc0ac4e38921d
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-01-29 11:15:24 -0300

    feat(repository): implement Product repository

---
+++ KluskaStore.Infrastructure/Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using KluskaStore.Application.Abstractions.Persistence;
+using KluskaStore.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace KluskaStore.Infrastructure.Persistence.Repositories;
+
+public sealed class ProductRepository(AppDbContext context) : IProductRepository
+{
+    public async Task<Product?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        => await context.Products.FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
+}
