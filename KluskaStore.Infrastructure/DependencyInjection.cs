492039e7478fb8b2554d3875b1de9f40208ac26f
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-01-29 11:16:53 -0300

    feat(DI): implement Infrastructure Dependency Injection

---
+++ KluskaStore.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using KluskaStore.Application.Abstractions.Persistence;
+using KluskaStore.Infrastructure.Persistence;
+using KluskaStore.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KluskaStore.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        services.AddDbContext<AppDbContext>(options => options.UseNpgsql(
+            configuration.GetConnectionString("Default")
+        ));
+
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+}
