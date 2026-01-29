c71156b237081a02b6ee07ec66ebbf81b902010b
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-01-29 11:12:56 -0300

    feat(db context): implement AppDbContext

---
+++ KluskaStore.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using KluskaStore.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace KluskaStore.Infrastructure.Persistence;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products => Set<Product>();
+}
