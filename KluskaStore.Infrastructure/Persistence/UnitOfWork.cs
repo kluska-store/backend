356deda12d16fe928160f390df56c6eac2d96a2b
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-01-29 11:16:08 -0300

    feat(infra): implement Unit of Work

---
+++ KluskaStore.Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using KluskaStore.Application.Abstractions.Persistence;
+
+namespace KluskaStore.Infrastructure.Persistence;
+
+public sealed class UnitOfWork(AppDbContext context) : IUnitOfWork
+{
+    public async Task CommitAsync(CancellationToken cancellationToken = default)
+        => await context.SaveChangesAsync(cancellationToken);
+}
