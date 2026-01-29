bd56d8727508214d57d27c94ad061d4e8cde19f1
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-01-29 17:16:07 -0300

    feat(migrations): add Initial migration, dependencies and Factories

---
+++ KluskaStore.Infrastructure/Persistence/AppDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace KluskaStore.Infrastructure.Persistence;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddUserSecrets<AppDbContext>(optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseNpgsql(configuration.GetConnectionString("Default"))
+            .Options;
+
+        return new AppDbContext(options);
+    }
+}
