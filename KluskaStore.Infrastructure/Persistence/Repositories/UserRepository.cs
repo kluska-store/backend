3c25bbd11de6eaa32282cd4bcadc2ed9587d2216
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-01-30 11:14:40 -0300

    feat(user repository): implement user repository and persisntece

---
+++ KluskaStore.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using KluskaStore.Application.Abstractions.Persistence;
+using KluskaStore.Domain.Entities.Accounts;
+using Microsoft.EntityFrameworkCore;
+
+namespace KluskaStore.Infrastructure.Persistence.Repositories;
+
+public sealed class UserRepository(AppDbContext context) : IUserRepository
+{
+    public async Task AddAsync(User user, CancellationToken cancellationToken = default)
+        => await context.Users.AddAsync(user, cancellationToken);
+
+    public async Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        => await context.Users.FirstOrDefaultAsync(u => u.Id == id, cancellationToken);
+
+    public async Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default)
+        => await context.Users.FirstOrDefaultAsync(u => u.Email.Value == email, cancellationToken);
+}
